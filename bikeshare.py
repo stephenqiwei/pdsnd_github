2ac8cd3d06dbca87d44e8347aa172b0f02448b7c
root <root@localhost>
2024-07-31 03:08:26 +0000

    :Factor: add load data section in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -64,7 +64,7 @@ def load_data(city, month, day):
     Args:
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+        (str) day - name  of the day of week to filter by, or "all" to apply no day filter
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
