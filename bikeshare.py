a887615a1fffce00e6e6556f90760318e22d326a
root <root@localhost>
2024-07-31 02:29:19 +0000

    Factor:add input weekday section in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
-    # TO DO: get user input for month (all, january, february, ... , june)
+    # TO DO: get  user input for month (all, january, february, ... , june)
 
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
 day=input("which weekday do you want search:all, monday, tuesday, wednesday, thursday, friday, saturday, sunday ?\n").lower()
