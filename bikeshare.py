08ce5f07d4dd15d82fb2c5ea4b68982e176be31c
root <root@localhost>
2024-07-31 02:31:36 +0000

    Factor: add load date section in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -59,7 +59,7 @@ def load_data(city, month, day):
     """
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified  city and filters by month and day if applicable.
 
     Args:
         (str) city - name of the city to analyze
