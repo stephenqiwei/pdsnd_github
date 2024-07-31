937d00e5bafc636ea5918d0b0808d25453ddb7b3
root <root@localhost>
2024-07-31 00:19:07 +0000

    add input city

--- bikeshare.py
+++ bikeshare.py
@@ -17,7 +17,15 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+ 
+    city=input("which city do you want search: chicago, new york city or washington?\n").lower()
+    while city!='chicago':
+        if city=='new york city':
+           break
+        if city=='washington':
+           break
+        print("Wrong input,please try again: chicago, new york city or washington")
+        city=input("which city do you want search:chicago, new york city or washington?\n").lower()
     # TO DO: get user input for month (all, january, february, ... , june)
 
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
